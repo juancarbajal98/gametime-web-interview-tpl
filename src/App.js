e4421ff5c7e81e5e5622edd81ca1879beb82660e
juancarbajal98 <juancarbajal98@gmail.com>
2022-05-23 14:50:44 -0700

    Fetching + parsing static oakland request

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect, useState } from 'react';
+import logo from './Gametime_logo.png';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+  let results = ``
+  useEffect(() => {
+    // get results
+    const loadResults = async() => {
+      const response = await axios.get('https://mobile-staging.gametime.co/v1/search?q=oakland')
+      // console.log(response.data)
+
+      // construct HTML to update view with results
+      let result_entity_pattern = {0: 'events', 1: 'performers', 2:'venues'}
+      for(let i =0; i <3;i++){
+        for(let j=0;j<3;j++){
+          let current_result = response.data[result_entity_pattern[i]][j]
+          if(current_result === undefined) continue
+          console.log(current_result)
+          switch(result_entity_pattern[i]){
+            case 'events':
+              results += `<img src="${current_result.performers[0].hero_image_url}">`
+              results += `<p>${current_result.event.name}</p>`
+              results += `<p>${current_result.venue.name}</p>`
+              break
+          case 'performers':
+            results += `<img src="${current_result.hero_image_url}">`
+            results += `<p>${current_result.name}</p>`
+            results += `<p>${current_result.category}</p>`
+            break
+          case 'venues':
+            results += `<img src="${current_result.image_url}">`
+            results += `<p>${current_result.name}</p>`
+            results += `<p>${current_result.city}</p>`
+            break
+          }
+        }
+      }
+    }
+    loadResults();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+      <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-searchBar">
+          <input className ="App-searchInput" type="text"></input>
+          <div className="App-searchResults">
+          {results}
+          </div>
+        </div>
       </header>
     </div>
   );
